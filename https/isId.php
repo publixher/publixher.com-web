026a773ab887388e85aede35eb84122843c090fa
강동균 <gangdong-gyun@gangdong-gyun-ui-MacBook-Pro.local>
2016-04-28 22:12:57 +0900

    isId 위치 바꿈

--- https/isId.php
+++ https/isId.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type:application/json");
 if (!empty($_GET)) {
-    require_once '../../conf/database_conf.php';
+    require_once '../conf/database_conf.php';
     $sql = "SELECT EMAIL FROM publixher.TBL_USER WHERE EMAIL=:EMAIL";
     $prepare=$db->prepare($sql);
     $prepare->bindValue(':EMAIL',$_GET['email'],PDO::PARAM_STR);
