5e63748375926fb650913356dbb31e93c6ead57b
강동균 <gangdong-gyun@gangdong-gyun-ui-MacBook-Pro.local>
2016-04-28 22:16:04 +0900

    dataconf 옮김

--- https/isId.php
+++ https/isId.php
@@ -1,7 +1,25 @@
 <?php
 header("Content-Type:application/json");
 if (!empty($_GET)) {
-    require_once '../conf/database_conf.php';
+    $dbServer = '127.0.0.1';
+    $dbUser='Kang';
+    $dbPass='!Pp999223';
+    $dbName='publixher';
+
+    $dsn="mysql:host={$dbServer};dbName={$dbName};,charset=utf8";
+
+    try {
+        //db연결하기
+        $db = new PDO($dsn, $dbUser, $dbPass);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->query("SET NAMES utf8;");
+
+
+    } catch (PDOException $e) {
+        echo '접속할 수 없습니다. 이유 : ' . h($e->getMessage());
+    }
+
     $sql = "SELECT EMAIL FROM publixher.TBL_USER WHERE EMAIL=:EMAIL";
     $prepare=$db->prepare($sql);
     $prepare->bindValue(':EMAIL',$_GET['email'],PDO::PARAM_STR);
