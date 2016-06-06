a0e6ad5d82d2a86c56db36255bd355c409bdbf8c
donggyun <ehdrbs0318@gmail.com>
2016-06-06 17:16:38 +0900

    비밀번호 바꿨음

--- conf/mongo_conf.php
+++ conf/mongo_conf.php
@@ -5,4 +5,4 @@
  * Date: 2016. 6. 6.
  * Time: 오후 3:13
  */
-$mongomanager=new MongoDB\Driver\Manager("mongodb://DongGyun:Sk763118!@localhost:27017/publixher");
+$mongomanager=new MongoDB\Driver\Manager("mongodb://DongGyun:Pp999223#@localhost:27017/publixher");
--- lib/content_explode.php
+++ lib/content_explode.php
@@ -1,7 +1,7 @@
 <?php
 function content_explode($body_text,$uid)
 {
-    $manager=new MongoDB\Driver\Manager("mongodb://DongGyun:Sk763118!@localhost:27017/publixher");
+    $manager=new MongoDB\Driver\Manager("mongodb://DongGyun:Pp999223#@localhost:27017/publixher");
     $words = preg_split("/[^a-zA-Z0-9_ㄱ-ㅎㅏ-ㅣ가-힣]/", $body_text);
     foreach($words as $key=>$word ){
         if(mb_strlen($word,"UTF-8")<2) unset($words[$key]);
