9acc8cafbedf8b3c1ff3b15c4b65bb4fb4148527
donggyun <ehdrbs0318@gmail.com>
2016-06-06 17:03:50 +0900

    몽고디비에 콘텐츠 piece 넣기

---
+++ lib/content_explode.php
@@ -0,0 +1,18 @@
+<?php
+function content_explode($body_text,$uid)
+{
+    $manager=new MongoDB\Driver\Manager("mongodb://DongGyun:Sk763118!@localhost:27017/publixher");
+    $words = preg_split("/[^a-zA-Z0-9_ㄱ-ㅎㅏ-ㅣ가-힣]/", $body_text);
+    foreach($words as $key=>$word ){
+        if(mb_strlen($word,"UTF-8")<2) unset($words[$key]);
+    }
+    $words = array_count_values($words);
+    arsort($words);
+    $pieces=array_slice($words,0,4);
+
+    $bulk=new MongoDB\Driver\BulkWrite;
+
+    $document=['id'=>$uid,'pieces'=>$pieces];
+    $bulk->insert($document);
+    $manager->executeBulkWrite('publixher.contents',$bulk);
+}
--- php/api/actionData.php
+++ php/api/actionData.php
@@ -1,3 +1,10 @@
 <?php
+require_once '../../conf/mongo_conf.php';
+$action=$_POST['action'];
 
-?>
+if($action=='readDone'){
+    $itemID=$_POST['itemID'];
+    $userID=$_POST['userID'];
+    $time = $_POST['time'];
+    
+}
