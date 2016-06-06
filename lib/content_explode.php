648274f1192a06289387656742d4228d42edf5c8
donggyun <ehdrbs0318@gmail.com>
2016-06-06 18:15:34 +0900

    사용자가 본거 기록

--- lib/content_explode.php
+++ lib/content_explode.php
@@ -1,24 +1,18 @@
 <?php
-function content_explode($body_text,$uid)
+function content_explode($body_text, $uid)
 {
-    $manager=new MongoDB\Driver\Manager("mongodb://DongGyun:Pp999223#@localhost:27017/publixher");
+    $manager = new MongoDB\Driver\Manager("mongodb://DongGyun:Pp999223#@localhost:27017/publixher");
     $words = preg_split("/[^a-zA-Z0-9_ㄱ-ㅎㅏ-ㅣ가-힣]/", $body_text);
-    foreach($words as $key=>$word ){
-        if(mb_strlen($word,"UTF-8")<2) unset($words[$key]);
+    foreach ($words as $key => $word) {
+        if (mb_strlen($word, "UTF-8") < 2) unset($words[$key]);
     }
     $words = array_count_values($words);
     arsort($words);
-    $pieces=array_slice($words,0,4);
+    $pieces = array_slice($words, 0, 4);
 
-    $bulk=new MongoDB\Driver\BulkWrite;
+    $bulk = new MongoDB\Driver\BulkWrite;
 
-    $document=['id'=>$uid,'pieces'=>$pieces];
+    $document = ['id' => $uid, 'pieces' => $pieces];
     $bulk->insert($document);
-    try {
-        $manager->executeBulkWrite('publixher.contents', $bulk);
-    }catch(MongoDB\Driver\Exception\BulkWriteException $e){
-        $msg=$e->getWriteResult();
-    }catch(MongoDB\Driver\Exception\Exception $e){
-        $msg=$e->getMessage();
-    }
+    $manager->executeBulkWrite('publixher.contents', $bulk);
 }
