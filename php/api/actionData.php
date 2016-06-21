e96462d2ec0f1a823da08580ce0043c3a6034ec5
donggyun <ehdrbs0318@gmail.com>
2016-06-21 17:15:32 +0900

    average_time,total_read 올림,등록시 몽고에서 들어감

--- php/api/actionData.php
+++ php/api/actionData.php
@@ -6,7 +6,26 @@ if ($action == 'readDone') {
     $itemID = $_POST['itemID'];
     $userID = $_POST['userID'];
     $time = $_POST['time'];
+    $total_read;
+    $average_time;
+    $average_time_new;
+    $total_time;
     $bulk = new MongoDB\Driver\BulkWrite;
     $bulk->update(['id' => $userID], ['$addToSet' => ['interest' => ['id'=>$itemID,'time' => $time,'when'=>time()]]], ['upsert' => true]);
     $result = $mongomanager->executeBulkWrite('publixher.user', $bulk);
+
+    $filter=['id'=>$itemID];
+    $options=['projection'=>['_id'=>0]];
+    $query=new MongoDB\Driver\Query($filter,$options);
+    $rows=$mongomanager->executeQuery('publixher.contents',$query);
+    foreach($rows as $row){
+        $total_read=$row->total_read?$row->total_read:0;
+        $average_time=$row->average_time?$row->average_time:false;
+    }
+    $total_time=$total_read*$average_time;
+    $total_read++;
+    $average_time_new=($total_time+$time)/$total_read;
+    $bulk = new MongoDB\Driver\BulkWrite;
+    $bulk->update(['id'=>$itemID],['$set'=>['average_time'=>$average_time_new,'total_read'=>$total_read]],['upsert'=>true]);
+    $mongomanager->executeBulkWrite('publixher.contents',$bulk);
 }
