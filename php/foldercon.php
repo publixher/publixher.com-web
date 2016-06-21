a42a5492b23ac15a7c01bcad2f73fc379cebf301
donggyun <ehdrbs0318@gmail.com>
2016-06-21 19:38:33 +0900

    구글 애널리틱스 바꿈

--- php/foldercon.php
+++ php/foldercon.php
@@ -93,18 +93,7 @@
     <link rel="stylesheet" href="/plugins/gifplayer-master/dist/gifplayer.css">
     <script src="/plugins/gifplayer-master/dist/jquery.gifplayer.js"></script>
     <!--    구글 애널리틱스-->
-    <script>(function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-            a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-        ga('create', 'UA-73277050-1', 'auto');
-        ga('send', 'pageview');</script>
+    <script>   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');    ga('create', 'UA-73277050-2', 'auto');   ga('send', 'pageview');  </script>
 </div>
 </body>
 </html>
