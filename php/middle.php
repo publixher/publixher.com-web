c6f10187ceb4558308f9697206f122f7d2abbd51
SUNGHO LEE <SUNGHO LEE>
2016-06-25 16:35:34 +0900

    공지사항 헤더 색 바꿈

--- php/middle.php
+++ php/middle.php
@@ -161,15 +161,15 @@
     <!--    각 카드가 하나의 아이템-->
 <!--    공지사항 시작-->
     <div class="item-for-sale card notice" id="1sdhOPm7Xg" style="display: block;">
-        <div class="header">
+        <div class="header" style="background-color: #434343;">
             <div class="item-profile-wrap"><img src="/img/crop50/e381c49473a8ecbd5d5265c05ea8590c.png"
                                                 class="profilepic"></div>
-            <div class="writer"><a style="font-weight: 700;">analograph</a>&nbsp;<span
+            <div class="writer"><a style="font-weight: 700; color: #e3e3e3;">analograph</a>&nbsp;<span
                     class="content-expose">전체공개</span></div>
-            <div class="title">Welcome to analograph</div>
+            <div class="title" style="color: #e3e3e3;">Welcome to analograph</div>
         </div>
         <div class="body">analograph에 찾아주신 모든 분들께 감사합니다!&nbsp;한 달 뒤, 모바일 앱과 함께 정식런칭을 계획하고 있으니, 그 전까지&nbsp;어떠한 컨텐츠든 자유롭게
-            올려주세요!<br><div style="display: block; width: 90%; height: 150px; overflow: hidden; border-radius: 15px; position: relative; margin: 0 auto; border: 1px solid #ddd;">
+            올려주세요!<br><br><div style="display: block; width: 90%; height: 150px; overflow: hidden; border-radius: 15px; position: relative; margin: 0 auto; border: 1px solid #ddd;">
                 <img src="/img/crop/efc1caf98a65cd6e7739bd650d2a62e1.png" class="BodyPic" style="position: absolute; top: 42%; left: 50%; transform: translate(-50%, -50%);
 ">
             </div><br><br></div>
