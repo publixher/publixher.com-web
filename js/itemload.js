34199b5f759249e93dbf4dc385c3550288adc620
SUNGHO LEE <SUNGHO LEE>
2016-07-04 16:47:05 +0900

    no message

--- js/itemload.js
+++ js/itemload.js
@@ -76,12 +76,7 @@ function itemForSaleLoad(write, ID, name, date, title, knock, price, comment, bo
     write += '<div class="item-profile-wrap"><img src="' + pic + '" class="profilepic"></div>';
     write += '<div class="writer"><a href="/profile/' + writer + '">'
     write += name + '</a>&nbsp;'
-    if (folderID) {
-        write += '<span class=content-date>'+date + '</span>&nbsp;<span class="content-folder"><a href="/folder/' + folderID + '">' + foldername + '</a></span>&nbsp;';
-    } else {
-        write += '<span class="content-date">'+date + '</span>&nbsp;';
-    }
-
+    write += '<span class="content-date">'+date + '</span>&nbsp;';
     switch (expose) {
         case "0":
             write += '<span class="content-expose">나만보기</span>';
@@ -93,6 +88,10 @@ function itemForSaleLoad(write, ID, name, date, title, knock, price, comment, bo
             write += '<span class="content-expose">전체공개</span>';
             break;
     }
+    if (folderID) {
+        write += '<span class="content-folder"><a href="/folder/' + folderID + '">' + foldername + '</a></span>&nbsp;';
+    }
+
     //카테고리 표시부분
     if(category!='SNS') {
         write+='<span class="content-category"><span class="item-category">'+category+'</span>';
