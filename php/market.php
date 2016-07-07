46ff4cb92578b06eb36d9c354661091da5ad7b4b
SUNGHO LEE <SUNGHO LEE>
2016-07-07 16:01:52 +0900

    no message

--- php/market.php
+++ php/market.php
@@ -1,8 +1,7 @@
 <script src="/js/best.js" type="text/javascript"></script>
-<div class="market" style="height: 99999px;">
+<div class="market">
     <div class="dropdown">
         <button class="btn btn-default dropdown-toggle" type="button" id="best-category" data-toggle="dropdown" aria-expanded="true">
-            카테고리
             <span class="caret"></span>
         </button>
         <div class="dropdown-menu" id="best-category-sel" role="menu" aria-labelledby="best-category">
@@ -162,9 +161,6 @@
                     </div>
                 </div>
             </div>
-            <a class="left carousel-control">
-                <span class="pubico pico-plus" id="go-market"></span>
-            </a>
             <a class="right carousel-control" href="#market-nav-carousel" role="button" data-slide="next">
                 전환
             </a>
