a8ab90f9967598842fa0f0a94a0e56fd0754c62e
SUNGHO LEE <SUNGHO LEE>
2016-07-08 14:21:57 +0900

    공지 프리뷰 수정

--- php/middle.php
+++ php/middle.php
@@ -167,7 +167,7 @@
                     class="content-expose">전체공개</span></div>
             <div class="title" style="color: #e3e3e3;">Welcome to analograph</div>
         </div>
-        <div class="body">analograph에 찾아주신 모든 분들께 감사합니다!<br><br>&nbsp;한 달 뒤, 모바일 앱과 함께 정식런칭을 계획하고 있으니 그 전까지&nbsp;어떠한 컨텐츠든 자유롭게
+        <div class="body"><br><br>analograph에 찾아주신 모든 분들께 감사합니다!<br><br>&nbsp;한 달 뒤, 모바일 앱과 함께 정식런칭을 계획하고 있으니 그 전까지&nbsp;어떠한 컨텐츠든 자유롭게
             올려주세요!<br><br>*이 글은 analograph에 대한 설명과 메뉴얼을 포함하고 있습니다.<br><br><div style="display: block; width: 90%; height: 150px; overflow: hidden; border-radius: 15px; position: relative; margin: 0 auto; border: 1px solid #ddd;">
                 <img src="/img/crop/efc1caf98a65cd6e7739bd650d2a62e1.png" class="BodyPic" style="position: absolute; top: 44%; left: 50%; transform: translate(-50%, -50%);
 ">
