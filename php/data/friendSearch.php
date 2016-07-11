5bc5d24977347a6066d76b326e7b84c2f2b4823e
donggyun <ehdrbs0318@gmail.com>
2016-07-11 19:35:08 +0900

    friendSearch

--- php/data/friendSearch.php
+++ php/data/friendSearch.php
@@ -9,3 +9,17 @@ header("Content-Type:application/json");
 require_once '../../conf/mongo_conf.php';
 
 $list=$_GET['list'];
+$fb_ids=array();
+$ids=array();
+foreach($list as $item){
+    $fb_ids[]=$item['id'];
+}
+$filter=array('facebook_id'=>array('$in'=>$fb_ids));
+$option=array('projection'=>array('id'=>1),'sort'=>array('_id'=>-1));
+$query=new MongoDB\Driver\Query($filter,$option);
+$cursor=$mongomanager->executeQuery('publixher.user',$query);
+
+foreach($cursor as $user){
+    $ids[]=$user->id;
+}
+$ids;
