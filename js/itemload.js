3d156168ede72e33fda50e614cdc6619c7b6d885
donggyun <ehdrbs0318@gmail.com>
2016-07-20 15:16:30 +0900

    처음 로딩시에 노크이미지 체크 제거

--- js/itemload.js
+++ js/itemload.js
@@ -65,7 +65,6 @@ function itemLoad(write, ID, name, date, knock, comment, preview, writer, folder
     if(knocked==1){
         write=$(write);
         write.find('.pico-knock').addClass('knocked');
-        write.find('img').addClass('knocked-image');
     }
     return write;
 }
@@ -142,7 +141,6 @@ function itemForSaleLoad(write, ID, name, date, title, knock, price, comment, bo
     if(knocked==1){
         write=$(write);
         write.find('.pico-knock').addClass('knocked');
-        write.find('img').addClass('knocked-image');
     }
     return write;
 }
