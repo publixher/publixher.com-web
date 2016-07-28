ed8cb0f00018e73fc0b28e5f16732ac911c70c32
donggyun <ehdrbs0318@gmail.com>
2016-07-28 15:18:17 +0900

    모바일 doubletap 이벤트 추가 및 적용

--- js/itemcard.js
+++ js/itemcard.js
@@ -15,8 +15,7 @@ $(document).ready(function () {
     }
 
     //더블클릭 노크 동작
-    $(document).on('dblclick','.body img',function(e){
-        console.log($(this))
+    $(document).on('dblclick doubletap','.body img',function(e){
         var $card=$(this).parents('.card');
         var $knock=$card.find('.knock');
         $knock.trigger('click');
@@ -1438,3 +1437,40 @@ function findIndex(array, attr, val) {
         }
     }
 }
+//더블탭 플러그인
+/*
+ * jQuery Double Tap
+ * Developer: Sergey Margaritov (sergey@margaritov.net)
+ * Date: 22.10.2013
+ * Based on jquery documentation http://learn.jquery.com/events/event-extensions/
+ */
+
+(function($){
+
+    $.event.special.doubletap = {
+        bindType: 'touchend',
+        delegateType: 'touchend',
+
+        handle: function(event) {
+            var handleObj   = event.handleObj,
+                targetData  = jQuery.data(event.target),
+                now         = new Date().getTime(),
+                delta       = targetData.lastTouch ? now - targetData.lastTouch : 0,
+                delay       = delay == null ? 300 : delay;
+
+            if (delta < delay && delta > 30) {
+                targetData.lastTouch = null;
+                event.type = handleObj.origType;
+                ['clientX', 'clientY', 'pageX', 'pageY'].forEach(function(property) {
+                    event[property] = event.originalEvent.changedTouches[0][property];
+                })
+
+                // let jQuery handle the triggering of "doubletap" event handlers
+                handleObj.handler.apply(this, arguments);
+            } else {
+                targetData.lastTouch = now;
+            }
+        }
+    };
+
+})(jQuery);
