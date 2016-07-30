a422ab34519f15c42807a408a0c28e7bff8a9769
donggyun <ehdrbs0318@gmail.com>
2016-07-30 15:15:08 +0900

    $_POST => $_GET

--- php/api/registConfirm.php
+++ php/api/registConfirm.php
@@ -4,12 +4,12 @@ require_once '../../conf/database_conf.php';
 require_once '../../lib/random_64.php';
 require_once '../../lib/Sendmail.php';
 //유효성 검사
-$email = $_POST['email'];
-$pass = $_POST['pass'];
-$name = $_POST['name'];
-$community = $_POST['community'];
+$email = $_GET['email'];
+$pass = $_GET['pass'];
+$name = $_GET['name'];
+$community = $_GET['community'];
 $msg='';
-$check_email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL); //옵션에 따라 유효성검사하는거 틀리면 false리턴하고 맞으면 그걸 리턴하는거
+$check_email = filter_var($_GET['email'], FILTER_VALIDATE_EMAIL); //옵션에 따라 유효성검사하는거 틀리면 false리턴하고 맞으면 그걸 리턴하는거
 $check_pass = preg_match("/^[[:alnum:]]{6,16}$/", $pass);
 
 //통과시 등록
@@ -22,13 +22,13 @@ if ($check_email && $check_pass) {
         $db->beginTransaction();
         $sql = "INSERT INTO publixher.TBL_USER(ID,EMAIL,PASSWORD,USER_NAME,SEX,BIRTH,COMMUNITY) VALUES (:ID,:EMAIL,:PASSWORD,:USER_NAME,:SEX,:BIRTH,:COMMUNITY)";
         $prepare = $db->prepare($sql);
-        $hash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+        $hash = password_hash($_GET['pass'], PASSWORD_DEFAULT);
         $prepare->bindValue(':ID', $id, PDO::PARAM_STR);
         $prepare->bindValue(':EMAIL', $check_email, PDO::PARAM_STR);
         $prepare->bindValue(':PASSWORD', $hash, PDO::PARAM_STR);
         $prepare->bindValue(':USER_NAME', $name, PDO::PARAM_STR);
-        $prepare->bindValue(':SEX', $_POST['sex'], PDO::PARAM_STR);
-        $prepare->bindValue(':BIRTH', $_POST['byear'] . $_POST['bmonth'] . $_POST['bday'], PDO::PARAM_STR);
+        $prepare->bindValue(':SEX', $_GET['sex'], PDO::PARAM_STR);
+        $prepare->bindValue(':BIRTH', $_GET['byear'] . $_GET['bmonth'] . $_GET['bday'], PDO::PARAM_STR);
         $prepare->bindValue(':COMMUNITY', $community);
         $prepare->execute();
         $seq=$db->lastInsertId();
@@ -54,8 +54,7 @@ if ($check_email && $check_pass) {
         echo $msg;
     }
 } else {
-    $msg=array("email"=>$email,"pass"=>$pass,"name"=>$name,"community"=>$community,"check_email"=>$check_email,"check_pass"=>$check_pass
-    ,"POST"=>$_POST,"GET"=>$_GET,'$_SERVER[\'HTTPS\']'=>$_SERVER['HTTPS']);
-    echo json_encode($msg,JSON_UNESCAPED_UNICODE);
+    $msg = '{"status":-2}';
+    echo $msg;
 }
 ?>
