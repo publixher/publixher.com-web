95a4b8968ea22810107c12b77f36cde735cff7ea
donggyun <ehdrbs0318@gmail.com>
2016-08-10 15:14:20 +0900

    푸시 장비 등록

--- php/data/itemAct.php
+++ php/data/itemAct.php
@@ -14,6 +14,35 @@ if (!isset($_POST['token']) AND !isset($_GET['token'])) {
     exit('부정한 조작이 감지되었습니다. case2 \n$_POST["token"] :' . $_POST['token'] . ' \n $_GET["token"] :' . $_GET['token'] . '$_SESSION :' . $_SESSION);
 }
 
+function send_notification ($tokens, $message)
+{
+    $url = 'https://fcm.googleapis.com/fcm/send';
+    $fields = array(
+        'registration_ids' => $tokens,
+        'data' => $message
+    );
+
+    $headers = array(
+        'Authorization:key =' . GOOGLE_API_KEY,
+        'Content-Type: application/json'
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+    $result = curl_exec($ch);
+    if ($result === FALSE) {
+        die('Curl failed: ' . curl_error($ch));
+    }
+    curl_close($ch);
+    return $result;
+}
+
 $act = $_POST['action'];
 if (!$act) {
     $act = $_GET['action'];
